25ed368e2e7a525c459e4f2e12ffd3a849f389ad
Pogchampgone <77293365+Pogchampgone@users.noreply.github.com>
2021-01-11 10:21:56 -0800

    Create Actualtt.lua

---
+++ Actualtt.lua
@@ -0,0 +1,14 @@
+local a = game.PlaceId
+if a == 4906760764 then
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pogchampgone/luashit/main/Proj.lua')))()
+else
+    if a == 6094061470 then
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/AltsegoD/script/master/ArcadeEmpire.lua')))()
+        else
+            print('Game Not Supported')
+    end
+end
+
+--tuen into a hub in the morening 
+
+--old game.GameId
