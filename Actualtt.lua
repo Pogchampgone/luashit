ff41f22738515ffa1ad2dfe6754b1958f9f668c6
Pogchampgone <77293365+Pogchampgone@users.noreply.github.com>
2021-01-12 12:22:45 -0800

    Update Actualtt.lua

--- Actualtt.lua
+++ Actualtt.lua
@@ -1,19 +1,23 @@
 local a = game.PlaceId
 print("loading...")
-if a == 4906760764 then
+if a == 4906760764 then --Jojo Game
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/Pogchampgone/luashit/main/Proj.lua')))()
 print("Finished Loading")
 else
-    if a == 6094061470 then
+    if a == 6094061470 then --Arcade Empire
         loadstring(game:HttpGet(('https://raw.githubusercontent.com/AltsegoD/script/master/ArcadeEmpire.lua')))()
 print("Finished Loading")
         else
-            if a == 537413528 then
+            if a == 537413528 then --Build a boat
 loadstring(game:HttpGet("https://dirtgui.xyz/ImageLoader.lua",true))()
 print("Finished Loading")
 else
-print("Game Not Supported")
-    end
+    if a == 1215581239 then --Doomspire
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Pogchampgone/luashit/main/Doomspire.lua",true))()
+        print("Finished Loading")
+    else
+        print("Game Not Supported")
+end
 end
 end
 
