73ab98c3acb8da6d3281a1e00cbdac3b3ac0063f
Pogchampgone <77293365+Pogchampgone@users.noreply.github.com>
2021-01-12 12:12:42 -0800

    Create Doomspire.lua

---
+++ Doomspire.lua
@@ -0,0 +1,76 @@
+local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/AikaV3rm/UiLib/master/Lib.lua')))()
+
+local w = library:CreateWindow("Crazy Hub") -- Creates the window
+
+local b = w:CreateFolder("Doomsipre") -- Creates the folder(U will put here your buttons,etc)
+
+b:Label("Scripts",{
+    TextSize = 25; -- Self Explaining
+    TextColor = Color3.fromRGB(255,255,255); -- Self Explaining
+    BgColor = Color3.fromRGB(69,69,69); -- Self Explaining
+    
+}) 
+
+b:Button("Inf Jump",function()
+    local Player = game:GetService'Players'.LocalPlayer;
+local UIS = game:GetService'UserInputService';
+
+_G.JumpHeight = 50;
+
+function Action(Object, Function) if Object ~= nil then Function(Object); end end
+
+UIS.InputBegan:connect(function(UserInput)
+    if UserInput.UserInputType == Enum.UserInputType.Keyboard and UserInput.KeyCode == Enum.KeyCode.Space then
+        Action(Player.Character.Humanoid, function(self)
+            if self:GetState() == Enum.HumanoidStateType.Jumping or self:GetState() == Enum.HumanoidStateType.Freefall then
+                Action(self.Parent.HumanoidRootPart, function(self)
+                    self.Velocity = Vector3.new(0, _G.JumpHeight, 0);
+                end)
+            end
+        end)
+    end
+end)
+end)
+b:Button("TP To Green",function()
+    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(0.899397671, 24, -131))
+end)
+
+b:Button("TP To Yellow",function()
+    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(130, 23, -0))
+end)
+b:Button("TP To Red",function()
+    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(-130, 23, -0))
+end)
+b:Button("TP To Blue",function()
+    game.Players.LocalPlayer.Character:MoveTo(Vector3.new(1, 23, 130))
+end)
+--130, 65, -0
+
+
+
+b:Label("By Pastebin#9785",{
+    TextSize = 25; -- Self Explaining
+    TextColor = Color3.fromRGB(255,255,255); -- Self Explaining
+    BgColor = Color3.fromRGB(69,69,69); -- Self Explaining
+    
+}) 
+b:DestroyGui()
+
+--[[
+How to refresh a dropdown:
+1)Create the dropdown and save it in a variable
+local yourvariable = b:Dropdown("Hi",yourtable,function(a)
+    print(a)
+end)
+2)Refresh it using the function
+yourvariable:Refresh(yourtable)
+How to refresh a label:
+1)Create your label and save it in a variable
+local yourvariable = b:Label("Pretty Useless NGL",{
+    TextSize = 25; -- Self Explaining
+    TextColor = Color3.fromRGB(255,255,255);
+    BgColor = Color3.fromRGB(69,69,69);
+})
+2)Refresh it using the function
+yourvariable:Refresh("Hello") It will only change the text ofc
+]]
